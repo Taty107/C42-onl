7f46dbb86b70cc4ec9f7ed46a20aeb70bd649846
Taty107 <tatyana.volynets.802@gmail.com>
2026-06-03 13:57:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        calculate();
+    }
+
+    public static void calculate(){
+        int b = 10;
+        int c = 25;
+        int a = 4*(b+c-1)/2;
+        System.out.println(a);
+    }
+}
