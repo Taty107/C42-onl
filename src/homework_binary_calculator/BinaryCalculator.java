8181c1ced1f38e09a04898206ee59bf470c83fc8
Taty107 <tatyana.volynets.802@gmail.com>
2026-08-13 02:58:14 +0300

    do some chenges

--- src/homework_binary_calculator/BinaryCalculator.java
+++ src/homework_binary_calculator/BinaryCalculator.java
@@ -9,9 +9,7 @@ import java.util.Scanner;
 public class BinaryCalculator {
     public static final String RESET = "\u001B[0m";
     public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
     private static String historyFile = "src/History_file.txt";
