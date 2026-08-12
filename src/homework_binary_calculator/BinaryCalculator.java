a6a3a3cc8c266596191186354d282df37879a60e
Taty107 <tatyana.volynets.802@gmail.com>
2026-08-13 02:56:28 +0300

    binary convertor is compleate)

--- src/homework_binary_calculator/BinaryCalculator.java
+++ src/homework_binary_calculator/BinaryCalculator.java
@@ -1,5 +1,9 @@
 package homework_binary_calculator;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BinaryCalculator {
@@ -10,6 +14,7 @@ public class BinaryCalculator {
     public static final String BLUE = "\u001B[34m";
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
+    private static String historyFile = "src/History_file.txt";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -21,15 +26,48 @@ public class BinaryCalculator {
         while (running) {
             int methodChoice = getMethodChoice(scanner);
             int number = getDecimalNumber(scanner);
+            String stepsText = "";
+            String binaryResult = "";
+            String methodName = "";
 
+            if (methodChoice == 1) {
+                methodName = "with using an array";
+                String[] stepsArray = new String[32];
+                int[] stepCount = new int[1];
+                binaryResult = convertUsingArray(number, stepsArray, stepCount);
+
+                for (int i = 0; i < stepCount[0]; i++) {
+                    stepsText += stepsArray[i] + "\n";
+                }
+            } else if (methodChoice == 2){
+                methodName = "with using recursion";
+
+                if (number == 0) {
+                    stepsText = "0 / 2 = 0, remainder 0\n";
+                    binaryResult = "0";
+                } else {
+                    stepsText = getStepsRecursive(number);
+                    binaryResult = convertRecursive(number);
+                }
+            }
+
+            printConsoleResult(number, methodName, stepsText, binaryResult);
+
+            saveResultToFile(number, methodChoice == 1 ? "array" : "recursive", binaryResult);
+
+            running = askToContinue(scanner);
+
+            System.out.println(CYAN + "~".repeat(70));
+            System.out.println("\nAll results are saved in the file " + RESET + YELLOW + historyFile + RESET + CYAN + ".");
+            System.out.println("The program has finished its operation." + RESET);
         }
     }
 
     private static int getMethodChoice(Scanner scanner) {
         while (true) {
             System.out.println("Choose the method for converting the number");
-            System.out.println("1 - using an array");
-            System.out.println("2 - using recursion");
+            System.out.println(PURPLE + "1 - using an array");
+            System.out.println("2 - using recursion" + RESET);
             System.out.print("Enter the option number: ");
             String inputChoice = scanner.nextLine().trim();
             if (inputChoice.equals("1") || inputChoice.equals("2")) {
@@ -62,6 +100,88 @@ public class BinaryCalculator {
             processSteps[0] = "0 / 2 = 0, remainder 0";
             stepCount[0] = 1;
             return "0";
+        }
+        int[] remainders = new int[32];
+        int count = 0;
+        int temp = number;
+
+        while (temp > 0) {
+            int quotient = temp / 2;
+            int remainder = temp % 2;
+
+            processSteps[stepCount[0]] = temp + " / 2 = " + quotient + ", remainder " + remainder;
+            stepCount[0]++;
+
+            remainders[count] = remainder;
+            count++;
+            temp = quotient;
+        }
+
+        String binaryResult = "";
+        for (int index = count - 1; index >= 0; index--) {
+            binaryResult += remainders[index];
+        }
+        return binaryResult;
+    }
+
+    private static String convertRecursive(int number) {
+        if (number == 0) {
+            return "";
+        }
+        return convertRecursive(number / 2) + (number % 2);
+    }
+
+    private static String getStepsRecursive(int number) {
+        if (number == 0) {
+            return "";
+        }
+        int quotient = number / 2;
+        int remainder = number % 2;
+        String processSteps = number + " / 2 = " + quotient + ", remainder " + remainder + "\n";
+        return processSteps + getStepsRecursive(quotient);
+    }
+
+    private static void printConsoleResult(int number, String methodName, String stepsText, String binaryResult) {
+        System.out.println(CYAN + "~".repeat(70) + RESET);
+        System.out.println("The original decimal number: " + number);
+        System.out.println("Method of converting: " + methodName);
+        System.out.println("Process converting: ");
+        System.out.println(stepsText);
+        System.out.println("Remains in reverse order: " + binaryResult);
+        System.out.println("The binary value of a number " + number + ": " + binaryResult);
+        System.out.println(CYAN +"~".repeat(70) + RESET);
+
+    }
+
+    private static void saveResultToFile(int number, String methodName, String binaryResult) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(historyFile, true))) {
+            writer.newLine();
+            writer.write("The original decimal number: " + number);
+            writer.newLine();
+            writer.write("Method of converting: " + methodName);
+            writer.newLine();
+            writer.write("Result: " + binaryResult);
+            writer.newLine();
+            writer.write("~".repeat(50));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static boolean askToContinue(Scanner scanner) {
+        while (true){
+            System.out.println("Do you want to enter another decimal number?");
+            System.out.println(PURPLE + "1 - yes");
+            System.out.println("2 - no" + RESET);
+            System.out.print("Please enter the option number: ");
+
+            String input = scanner.nextLine();
+            if (input.equals("1")) {
+                return true;
+            } else if (input.equals("2")){
+                return false;
+            }
+            System.out.println(RED + "Error!! An incorrect value has been entered. Please enter 1 or 2." + RESET);
         }
     }
 }
